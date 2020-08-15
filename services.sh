70ad9a2ec7aaf598aecc814dc66f2ce8b43aa9d9
Matsushin <matsu1983@gmail.com>
2020-08-15 12:19:12 +0900

    コンパイル

--- services.sh
+++ services.sh
@@ -12,5 +12,6 @@ bundle install
 bundle exec rails db:create
 bundle exec rails db:migrate
 
+RUN RAILS_ENV=${RAILS_ENV} bundle exec rake assets:precompile
 bundle exec rails s -p 3000 -b '0.0.0.0'
 
