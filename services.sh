583f5a538494637b3b00ea0fe9b0343ddc01df4d
Matsushin <matsu1983@gmail.com>
2020-08-15 12:53:21 +0900

    微修正

--- services.sh
+++ services.sh
@@ -13,6 +13,6 @@ bundle exec rails db:create
 bundle exec rails db:migrate
 bundle exec rails db:seed
 
-RUN RAILS_ENV=${RAILS_ENV} bundle exec rake assets:precompile
+RAILS_ENV=${RAILS_ENV} bundle exec rake assets:precompile
 bundle exec rails s -p 3000 -b '0.0.0.0'
 
